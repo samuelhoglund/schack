bd181fa96e838d92d2c29e1877c05ee17cb0cc38
samuelhoglund <98042026+samuelhoglund@users.noreply.github.com>
2022-12-26 20:00:05 +0100

    tornet klart

---
+++ Range.java
@@ -0,0 +1,47 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+// From https://stackoverflow.com/questions/16570091/for-loop-like-python-range-function 
+
+class Range implements Iterable<Integer> {
+
+    private int start = 0;
+    private int limit;
+
+    public Range(int limit) {
+        this.limit = limit;
+    }
+
+    public Range(int start, int limit) {
+        this.start = start;
+        this.limit = limit;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        final int max = limit;
+        return new Iterator<Integer>() {
+
+            private int current = start;
+
+            @Override
+            public boolean hasNext() {
+                return current < max;
+            }
+
+            @Override
+            public Integer next() {
+                if (hasNext()) {
+                    return current++;   
+                } else {
+                    throw new NoSuchElementException("Range reached the end");
+                }
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("Can't remove values from a Range");
+            }
+        };
+    }
+}
--- Rook.java
+++ Rook.java
@@ -10,9 +10,37 @@ public class Rook extends Piece {
 
     @Override
     boolean moveOK(oldSquare s1, oldSquare s2, oldSquare[][] board) {
-        // TODO Auto-generated method stub
-        return false;
+        System.out.println("i rook");
+        int start = 0; int end = 0;
+
+        // base case
+        if (s2.hasPiece() && s2.getPiece().color==this.color) { return false; }
+        else if (((s2.x == s1.x+1 | s2.x == s1.x-1) & s2.y == s1.y) || ((s2.y == s1.y+1 | s2.y == s1.y-1) & s2.x == s1.x)) {    // one square away
+            if (s2.hasPiece() && s2.getPiece().color==this.color) {  // if there is ally on s2, move is not accepted
+                return false; }
+            return true;
+        }
+
+        if (s1.x == s2.x) {
+            // same row, iterate through squares inbetween
+            if (s1.y>s2.y) { start = s2.y+1; end = s1.y; }
+            else if (s1.y<s2.y) { start = s1.y+1; end = s2.y; }
+            for (int i : new Range(start, end)) {
+                if (board[s1.x][i].hasPiece()) { return false; }
+            }
+            return true;
+        }
+        else if (s1.y == s2.y) {
+            // same col, itrerate through squares inbetween
+            if (s1.x>s2.x) { start = s2.x+1; end = s1.x; }
+            else if (s1.x<s2.x) { start = s1.x+1; end = s2.x; }
+
+            for (int i : new Range(start, end)) {
+                if (board[i][s1.y].hasPiece()) { return false; }
+            }
+            return true;
+        }
+        return false;  
     }
 
-    
 }
